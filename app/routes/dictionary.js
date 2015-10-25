7d744667b256620c9933604d2a73abc133ae551c
Jain, Mayur Mayur.Jain@parc.com <Mayur.Jain@parc.com>
2015-10-24 17:41:11 -0700

    invalid keyword error bug fix, send 404

--- app/routes/dictionary.js
+++ app/routes/dictionary.js
@@ -8,6 +8,7 @@ var unirest = require('unirest');
 exports.getWordDesc = function (req, res) {
     var Card = req.app.db.models.Card;
     var keyword = req.query.keyword;
+    var colors = ['red', 'blue', 'green'];
 
     Card.findOne({"keyword": keyword}, function (err, card) {
         if (err) {
@@ -15,33 +16,39 @@ exports.getWordDesc = function (req, res) {
             res.status(204).send('card error');
         }
 
-        if(card){
-           console.log(card);
+        if (card) {
+            console.log(card);
             res.status(200).send(card);
-        }else{
+        } else {
             // These code snippets use an open-source library. http://unirest.io/nodejs
-            unirest.get("https://wordsapiv1.p.mashape.com/words/"+keyword+"/definitions")
+            unirest.get("https://wordsapiv1.p.mashape.com/words/" + keyword + "/definitions")
                 .header("X-Mashape-Key", "q3YhYwtuqVmshHEKKF1Nhpr3DYWNp1CN4EYjsnUdlujyx7tgj1")
                 .header("Accept", "application/json")
                 .end(function (result) {
                     //console.log(result);
                     //console.log(result.status, result.headers, result.body);
 
-                    var newCard = new Card ({
-                        "keyword": keyword,
-                        "description": result.body.definitions[0].definition,
-                        "image": "http://ummeedhyderabad.com/images/upload/default.jpg",
-                        "source_url": "source_url",
-                        "color": "red"
-                    });
+                    if(result.body.definitions != undefined){
+                        var newCard = new Card({
+                            "keyword": keyword,
+                            "description": result.body.definitions[0].definition,
+                            "image": "http://ummeedhyderabad.com/images/upload/default.jpg",
+                            "source_url": "source_url",
+                            "color": colors[Math.floor(Math.random() * colors.length)]
+                        });
 
-                    newCard.save(function(err, newcard){
-                        if(err){
-                            console.log("error: "+ err);
-                        }
-                        console.log(newcard);
-                        res.status(200).send(newcard);
-                    });
+                        newCard.save(function (err, newcard) {
+                            if (err) {
+                                console.log("error: " + err);
+                            }
+                            console.log(newcard);
+                            res.status(200).send(newcard);
+                        });
+                    }
+                    else{
+                        console.log("Word not found.");
+                        res.status(404).send("Word not found.");
+                    }
                 });
         }
     });
@@ -51,13 +58,13 @@ exports.getWordDesc = function (req, res) {
 exports.getAllCards = function (req, res) {
     var Card = req.app.db.models.Card;
 
-    Card.find({}).sort([['created_at', -1]]).exec( function (err, card) {
+    Card.find({}).sort([['created_at', -1]]).exec(function (err, card) {
         if (err) {
             console.log('Error in finding card error: ' + err);
             res.status(204).send('card error');
         }
 
-        if(card){
+        if (card) {
             console.log(card);
             res.status(200).send(card);
         }
