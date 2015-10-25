a5d1039f37e1004c266278876afe591749421c66
Jain, Mayur Mayur.Jain@parc.com <Mayur.Jain@parc.com>
2015-10-25 13:39:32 -0700

    added CORS allowed methods

--- app/middleware/CORSMiddleware.js
+++ app/middleware/CORSMiddleware.js
@@ -5,6 +5,7 @@ exports = module.exports = function(app) {
         res.header('Access-Control-Allow-Origin', '*');
 		res.header('Access-Control-Allow-Headers', 'Content-Type,X-Requested-With');
 		res.header('Access-Control-Allow-Credentials', 'true');
+        res.header('Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE');
 		next();
 	});
 
